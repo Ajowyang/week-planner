6e487c93a43db6f75a6507867d2e0831dae4dc2e
Samuel Castaneda <yugiohcoolguy@gmail.com>
2024-08-02 20:50:26 +0000

    7days......

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// interface DayPlan {
+//   time: string;
+//   day: string;
+//   event: string;
+// }
+// const dayPlan: DayPlan = {
+//   time: '',
+//   day: '',
+//   event: '',
+// };
+// Code for the modal
 var $modal = document.querySelector('.event-modal');
 if (!$modal) throw new Error('dialog query failed!');
 var $newEventButton = document.querySelector('.new-event-button');
@@ -12,25 +23,81 @@ $dismissModal.addEventListener('click', function () {
 });
 // Code for switching the table based on day of the week
 var $tableDay = document.querySelector('#chooseDay');
+var $monTable = document.querySelector('.monTable');
+var $tueTable = document.querySelector('.tueTable');
+var $wedTable = document.querySelector('.wedTable');
+var $thurTable = document.querySelector('.thurTable');
+var $friTable = document.querySelector('.friTable');
+var $satTable = document.querySelector('.satTable');
+var $sunTable = document.querySelector('.sunTable');
 if (!$tableDay) throw new Error('#chooseDay query failed!');
 // let value = $tableDay.value;
 $tableDay.addEventListener('change', function () {
-  var dayString = $tableDay.options[$tableDay.selectedIndex].text;
-  console.log(dayString);
+  var dayString = $tableDay.options[$tableDay.selectedIndex].value;
   switch (dayString) {
     case 'monday':
+      $monTable.setAttribute('class', 'unhidden');
+      $tueTable.setAttribute('class', 'hidden');
+      $wedTable.setAttribute('class', 'hidden');
+      $thurTable.setAttribute('class', 'hidden');
+      $friTable.setAttribute('class', 'hidden');
+      $satTable.setAttribute('class', 'hidden');
+      $sunTable.setAttribute('class', 'hidden');
       break;
     case 'tuesday':
+      $monTable.setAttribute('class', 'hidden');
+      $tueTable.setAttribute('class', 'unhidden');
+      $wedTable.setAttribute('class', 'hidden');
+      $thurTable.setAttribute('class', 'hidden');
+      $friTable.setAttribute('class', 'hidden');
+      $satTable.setAttribute('class', 'hidden');
+      $sunTable.setAttribute('class', 'hidden');
       break;
     case 'wednesday':
+      $monTable.setAttribute('class', 'hidden');
+      $tueTable.setAttribute('class', 'hidden');
+      $wedTable.setAttribute('class', 'unhidden');
+      $thurTable.setAttribute('class', 'hidden');
+      $friTable.setAttribute('class', 'hidden');
+      $satTable.setAttribute('class', 'hidden');
+      $sunTable.setAttribute('class', 'hidden');
       break;
     case 'thursday':
+      $monTable.setAttribute('class', 'hidden');
+      $tueTable.setAttribute('class', 'hidden');
+      $wedTable.setAttribute('class', 'hidden');
+      $thurTable.setAttribute('class', 'unhidden');
+      $friTable.setAttribute('class', 'hidden');
+      $satTable.setAttribute('class', 'hidden');
+      $sunTable.setAttribute('class', 'hidden');
       break;
     case 'friday':
+      $monTable.setAttribute('class', 'hidden');
+      $tueTable.setAttribute('class', 'hidden');
+      $wedTable.setAttribute('class', 'hidden');
+      $thurTable.setAttribute('class', 'hidden');
+      $friTable.setAttribute('class', 'unhidden');
+      $satTable.setAttribute('class', 'hidden');
+      $sunTable.setAttribute('class', 'hidden');
       break;
     case 'saturday':
+      $monTable.setAttribute('class', 'hidden');
+      $tueTable.setAttribute('class', 'hidden');
+      $wedTable.setAttribute('class', 'hidden');
+      $thurTable.setAttribute('class', 'hidden');
+      $friTable.setAttribute('class', 'hidden');
+      $satTable.setAttribute('class', 'unhidden');
+      $sunTable.setAttribute('class', 'hidden');
       break;
     case 'sunday':
+      $monTable.setAttribute('class', 'hidden');
+      $tueTable.setAttribute('class', 'hidden');
+      $wedTable.setAttribute('class', 'hidden');
+      $thurTable.setAttribute('class', 'hidden');
+      $friTable.setAttribute('class', 'hidden');
+      $satTable.setAttribute('class', 'hidden');
+      $sunTable.setAttribute('class', 'unhidden');
       break;
   }
 });
+// Code to render the the new html lines based off the code.
--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+// interface DayPlan {
+//   time: string;
+//   day: string;
+//   event: string;
+// }
+
+// const dayPlan: DayPlan = {
+//   time: '',
+//   day: '',
+//   event: '',
+// };
+
+// Code for the modal
 const $modal = document.querySelector('.event-modal') as HTMLDialogElement;
 if (!$modal) throw new Error('dialog query failed!');
 
@@ -21,27 +34,85 @@ $dismissModal.addEventListener('click', function () {
 
 // Code for switching the table based on day of the week
 const $tableDay = document.querySelector('#chooseDay') as HTMLSelectElement;
+const $monTable = document.querySelector('.monTable');
+const $tueTable = document.querySelector('.tueTable');
+const $wedTable = document.querySelector('.wedTable');
+const $thurTable = document.querySelector('.thurTable');
+const $friTable = document.querySelector('.friTable');
+const $satTable = document.querySelector('.satTable');
+const $sunTable = document.querySelector('.sunTable');
 if (!$tableDay) throw new Error('#chooseDay query failed!');
 
 // let value = $tableDay.value;
 
 $tableDay.addEventListener('change', function () {
-  const dayString = $tableDay.options[$tableDay.selectedIndex].text;
-  console.log(dayString);
+  const dayString = $tableDay.options[$tableDay.selectedIndex].value;
+
   switch (dayString) {
     case 'monday':
+      $monTable.setAttribute('class', 'unhidden');
+      $tueTable.setAttribute('class', 'hidden');
+      $wedTable.setAttribute('class', 'hidden');
+      $thurTable.setAttribute('class', 'hidden');
+      $friTable.setAttribute('class', 'hidden');
+      $satTable.setAttribute('class', 'hidden');
+      $sunTable.setAttribute('class', 'hidden');
       break;
     case 'tuesday':
+      $monTable.setAttribute('class', 'hidden');
+      $tueTable.setAttribute('class', 'unhidden');
+      $wedTable.setAttribute('class', 'hidden');
+      $thurTable.setAttribute('class', 'hidden');
+      $friTable.setAttribute('class', 'hidden');
+      $satTable.setAttribute('class', 'hidden');
+      $sunTable.setAttribute('class', 'hidden');
       break;
     case 'wednesday':
+      $monTable.setAttribute('class', 'hidden');
+      $tueTable.setAttribute('class', 'hidden');
+      $wedTable.setAttribute('class', 'unhidden');
+      $thurTable.setAttribute('class', 'hidden');
+      $friTable.setAttribute('class', 'hidden');
+      $satTable.setAttribute('class', 'hidden');
+      $sunTable.setAttribute('class', 'hidden');
       break;
     case 'thursday':
+      $monTable.setAttribute('class', 'hidden');
+      $tueTable.setAttribute('class', 'hidden');
+      $wedTable.setAttribute('class', 'hidden');
+      $thurTable.setAttribute('class', 'unhidden');
+      $friTable.setAttribute('class', 'hidden');
+      $satTable.setAttribute('class', 'hidden');
+      $sunTable.setAttribute('class', 'hidden');
       break;
     case 'friday':
+      $monTable.setAttribute('class', 'hidden');
+      $tueTable.setAttribute('class', 'hidden');
+      $wedTable.setAttribute('class', 'hidden');
+      $thurTable.setAttribute('class', 'hidden');
+      $friTable.setAttribute('class', 'unhidden');
+      $satTable.setAttribute('class', 'hidden');
+      $sunTable.setAttribute('class', 'hidden');
       break;
     case 'saturday':
+      $monTable.setAttribute('class', 'hidden');
+      $tueTable.setAttribute('class', 'hidden');
+      $wedTable.setAttribute('class', 'hidden');
+      $thurTable.setAttribute('class', 'hidden');
+      $friTable.setAttribute('class', 'hidden');
+      $satTable.setAttribute('class', 'unhidden');
+      $sunTable.setAttribute('class', 'hidden');
       break;
     case 'sunday':
+      $monTable.setAttribute('class', 'hidden');
+      $tueTable.setAttribute('class', 'hidden');
+      $wedTable.setAttribute('class', 'hidden');
+      $thurTable.setAttribute('class', 'hidden');
+      $friTable.setAttribute('class', 'hidden');
+      $satTable.setAttribute('class', 'hidden');
+      $sunTable.setAttribute('class', 'unhidden');
       break;
   }
 });
+
+// Code to render the the new html lines based off the code.
