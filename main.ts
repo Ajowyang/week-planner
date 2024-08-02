f724a93a69d7cfe9e4c19154e1109b8c177f9291
Samuel Castaneda <yugiohcoolguy@gmail.com>
2024-08-02 21:31:59 +0000

    Rendering Message......

--- main.ts
+++ main.ts
@@ -132,3 +132,29 @@ $tableDay.addEventListener('change', function () {
 });
 
 // Code to render the the new html lines based off the code.
+// const $trList = document.querySelector('tr-list');
+
+// function renderTR(data: DayPlan): unknown{
+//   const $tdTime = document.createElement('td');
+//   $tdTime.setAttribute('class', 'time-table');
+//   $tdTime.textContent = data.time;
+
+//   const $tdEvent = document.createElement('td');
+//   $tdEvent.setAttribute('class', 'event-table');
+//   $tdEvent.textContent = data.event;
+
+//   const $tdButtonEdit = document.createElement('td');
+//   $tdButtonEdit.setAttribute('class', 'button-edit');
+
+//   const $tdButtonDel = document.createElement('td');
+//   $tdButtonDel.setAttribute('class', 'button-delete');
+
+//   const $buttonEdit = document.createElement('button');
+//   $buttonEdit.setAttribute('class', 'delete');
+//   $buttonEdit.setAttribute('type', 'submit');
+
+//   const $buttonDel = document.createElement('button');
+//   $buttonDel.setAttribute('class', 'delete');
+//   $buttonDel.setAttribute('type', 'submit');
+
+//
