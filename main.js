4ef20af9e3ede9b3f97550cfa5b80f274abad724
ajowyang <andrewowyang875@gmail.com>
2024-08-02 18:46:43 +0000

    worked on show/hide modal functionality

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-var $newEventButton = document.querySelector('.new-event-button');
+var $modal = document.querySelector('.event-modal');
+if (!$modal) throw new Error('dialog query failed!');
+var $newEventButton = document.querySelector('.new-event.button');
 if (!$newEventButton) throw new Error('.new-event-button query failed!');
-$newEventButton.addEventListener('click', function () {});
+var $dismissModal = document.querySelector('.dismissModal');
+if (!$dismissModal) throw new Error('.dismissModal query failed!');
+$newEventButton.addEventListener('click', function () {
+  $modal.showModal();
+});
+$dismissModal.addEventListener('click', function () {
+  $modal.close();
+});
--- main.ts
+++ main.ts
@@ -1,4 +1,20 @@
-const $newEventButton = document.querySelector('.new-event-button');
+const $modal = document.querySelector('.event-modal') as HTMLDialogElement;
+if (!$modal) throw new Error('dialog query failed!');
+
+const $newEventButton = document.querySelector(
+  '.new-event.button',
+) as HTMLButtonElement;
 if (!$newEventButton) throw new Error('.new-event-button query failed!');
 
-$newEventButton.addEventListener('click', function () {});
+const $dismissModal = document.querySelector(
+  '.dismissModal',
+) as HTMLButtonElement;
+if (!$dismissModal) throw new Error('.dismissModal query failed!');
+
+$newEventButton.addEventListener('click', function () {
+  $modal.showModal();
+});
+
+$dismissModal.addEventListener('click', function () {
+  $modal.close();
+});
