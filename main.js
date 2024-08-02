2b4cab1aa78d95643b849392bda55199337fe697
ajowyang <andrewowyang875@gmail.com>
2024-08-02 17:35:58 +0000

    added some styling

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const $newEventButton = document.querySelector('.new-event-button');
+var $newEventButton = document.querySelector('.new-event-button');
 if (!$newEventButton) throw new Error('.new-event-button query failed!');
-
 $newEventButton.addEventListener('click', function () {});
